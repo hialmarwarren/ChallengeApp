3e2a583dec763a76d5d22bd269227ded9804c25c
hialmarwarren <123904804+hialmarwarren@users.noreply.github.com>
2023-03-21 22:19:19 +0100

    Dzień 6

---
+++ Employee.cs
@@ -0,0 +1,27 @@
+﻿namespace Project;
+    internal class Employee
+    {
+        private List<int> score = new List<int>();
+        public Employee(int number, string name, string surname, int age)
+        {
+            this.UserId = number;
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+        public int UserId { get; private set; }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+        public void AddScore(int score)
+        {
+            this.score.Add(score);
+        }
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Project;
+using System.Globalization;
+
+Console.Write("podaj ilość użytkowników: ");
+var number = int.Parse(Console.ReadLine());
+Console.Write("Podaj liczbę ocen: ");
+var notes = int.Parse(Console.ReadLine());
+List<Employee> userList = new List<Employee>();
+int[] score = new int[number];
+for (int i = 0; i < number; i++)
+{
+    var n = i + 1;
+    Console.WriteLine("Podaj dane " + n + " użytkownika:");
+    Console.Write("Imię: ");
+    var name = Console.ReadLine();
+    Console.Write("Nazwisko: ");
+    var surname = Console.ReadLine();
+    Console.Write("Wiek w latach: ");
+    var age = int.Parse(Console.ReadLine());
+    userList.Add(new Employee(i, name, surname, age));
+    Console.Write("Wprowadź kolejno " + notes + " ocen/y pracownika, wartości całkowite w skali od 1 do 10: ");
+    for (int j = 0; j < notes; j++)
+    {
+        userList[i].AddScore(int.Parse(Console.ReadLine()));
+        Console.Write(" ");
+    }
+    score[i] = userList[i].Result;
+}
+var value = 0;
+var k = 0;
+for (int i = 0; i < number; i++)
+{
+    if (score[i] > value)
+    {
+        value = score[i];
+        k = i;
+    }
+}
+Console.WriteLine("Najwięcej punktów: " + userList[k].Result + ", otrzymał/a " + userList[k].Name + " " + userList[k].Surname + ", lat " + userList[k].Age);
+Console.WriteLine("Naciśnij dowolny klawisz");
+Console.ReadKey();
